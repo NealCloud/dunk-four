3f5d12c16fed12f40597c6210529fcc494f0c571
Neal Cloud <neal.cloud@gmail.com>
2016-02-10 11:25:52 -0800

    vertical check function - Neal

---
+++ neal.js
@@ -0,0 +1,39 @@
+/**
+ * Created by Mad Martigan on 2/10/2016.
+ */
+function vertical(board){
+    var x; var o;
+    var bLen = board.length;
+    for(var i = 0;i < bLen; i++){
+        x = 0;
+        o = 0;
+        for(var j = 0; j < bLen; j++){
+            if(board[j][i] == "x"){
+                x += 1;
+            }
+            else if(board[j][i] == "o"){
+                o += 1;
+            }
+        }
+        if(x == bLen){
+            return "X";
+        }
+        else if(o == bLen){
+            return "O";
+        }
+    }
+    return false;
+}
+
+var winCheck = [vertical, vertical];
+
+function checkWin(){
+    for(var i = 0; i < winCheck.length; i++){
+        var a = winCheck[0](b);
+        if(a) return a;
+    }
+    return false;
+}
+
+b = [["x","x","x"],["","",""],["x","x","x"]];
+console.log(checkWin());
