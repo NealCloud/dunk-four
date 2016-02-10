7d6a676df2dc33175b42fcea1c50b191647aab0d
Mark Jurkiewicz <markajurkiewicz@gmail.com>
2016-02-10 15:27:00 -0800

    TicTacToe Game rough commit

--- tictac.js
+++ tictac.js
@@ -1,3 +1,6 @@
+//horizontal function
+
+
 function horizontal (board) {
     var x; var o;
     var bLen = board.length;
@@ -29,7 +32,6 @@ function horizontal (board) {
 
 
 
-
 function vertical(board){
     var x; var o;
     var bLen = board.length;
@@ -54,15 +56,64 @@ function vertical(board){
     return false;
 }
 
-var winCheck = [vertical, vertical];
+function leftToRightDiagonal(array) {
+    var counterX = 0;
+    var counterO = 0;
+    for (var i = 0; i < 1; i++) {
+        for (var j = 0; j < array.length; j++) {
+            if (array[i + j][j] == "x") {
+                counterX++;
+            }
+            else if(array[i + j][j] == "o"){
+                counterO++;
+            }
+        }
+    }
+
+    if(counterO == array.length){
+        return("O wins");
+    }
+    else if(counterX == array.length){
+        return("X wins");
+    }
+    else{
+        return false;
+    }
+}
+
+function rightToLeftDiagonal(array) {
+    var counterX = 0;
+    var counterO = 0;
+    for (var i = array.length - 1; i < array.length; i++) {
+        for (var j = 0; j < array.length; j++) {
+            if(array[i - j][j] == "o") {
+                counterO++;
+            }
+            else if(array[i - j][j] == "x"){
+                counterX++;
+            }
+        }
+    }
+    if(counterO == array.length){
+       return("O wins");
+    }
+    else if(counterX == array.length){
+        return("X wins");
+    }else{
+        return false;
+    }
+}
+
+
+var winCheck = [vertical, horizontal, rightToLeftDiagonal, leftToRightDiagonal];
 
 function checkWin(){
     for(var i = 0; i < winCheck.length; i++){
-        var a = winCheck[0](b);
-        if(a) return a;
+        var a = winCheck[i](b);
+        if(a) return a + i;
     }
     return false;
 }
 
-b = [["x","x","x"],["","",""],["x","x","x"]];
+b = [["o","","x"],["o","x",""],["x","x","o"]];
 console.log(checkWin());
