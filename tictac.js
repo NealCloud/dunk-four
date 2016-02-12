e1d19452fa22474b1271ca505cb7f74308d575e0
Mark Jurkiewicz <markajurkiewicz@gmail.com>
2016-02-12 08:40:03 -0800

    reorganized

--- tictac.js
+++ tictac.js
@@ -26,6 +26,21 @@ var currentBox;
 var countdownClock = 0;
 var notStarted = true;
 
+
+//toggles the shot modal which allows user to make a shot
+function modalActive(){ //no returns, utility
+    var modal = $("#mode0" ); //jquery method to check if hidden
+    if ( modal.is( ":hidden" ) ) {
+        // pointing to a jquery selector in a variable modal previously declared
+        modal.css( "display", "block" );
+    }
+    else{
+        // pointing to a jquery selector in a variable modal previously declared
+        modal.css( "display", "none");
+    }
+}
+//takes in the text color and sound to display;
+
 function countdown(duration, display, callback){
     var timer, minutes, seconds;
 
@@ -50,88 +65,83 @@ function countdown(duration, display, callback){
     }, 1000);
 }
 
-function createBoardArray(number){
-    var gameArray = [];
-
-    for (var i = 0; i < number; i++) {
-        var pushArray = [];
-        //gameArray.push(pushArray);
-        for(var j = 0; j < number; j++) {
-
-            pushArray.push("");
-        }
-
-        gameArray.push(pushArray);
+function randomAlert(com){
+    if(com == "good"){
+        var r = Math.floor(Math.random()* alert.length);
+        displayAlert(alert[r][0],"success", alert[r][1]);
     }
-    return gameArray;
+    else{
+        var r = Math.floor(Math.random()* wiff.length);
+        displayAlert(wiff[r][0],"warn" );
+    }
 }
 
-// click the box function
-function clicked(targ) {
-//        takes element clicked id
-    console.log("clicked " + targ);
-//        splits id into row and column according to array position
-    //    calls the function checkClicked to see if a box was already clicked
-    if (!$(targ).html()) {
-        currentBox = targ;
-        createShotAttempt();
-        // return true if there is already text in the box
+//takes in the text color and sound to display;
+function displayAlert(text, type, sound){
+    var message = null;
+    if(type == "warn"){
+        // pointing to a jquery selector in a variable message previously declared
+        message = $("#alert");
+        message.removeClass("success"); //toggling classes
+        message.addClass("warning");
     }
-    else {
-        console.log("filled");
+    else{
+        // pointing to a jquery selector in variable message previously declared
+        message = $("#alert");
+        message.removeClass("warning");
+        message.addClass("success")
+    }
+    if(sound){
+        sound.play(); // variable sound calls on play audio function
+    }
+    $("#alert h1").text(text);  //displays commentator message
+}
+
+function winAnimation(){ // displays which player wins and displays crowd imgs and audio
+    //animates images
+    if(player1score > player2score){
+        $(".board").text("HOME TEAM WINS!!!");
+    }
+    else if(player1score < player2score){
+        $(".board").text("AWAY TEAM WINS!!!");
+    }
+    else{
+        $(".board").text("ITS A DRAW");
         return;
     }
-}
-function shotSuccess(targ){
-    var id = $(targ).attr("id");
-    var row = id[0];
-    var col = id[1];
-//        use the currentSymbol the mark the box
-    $(targ).html(currentSymbol);
+    $("#alert").slideUp("slow"); // jquery method which animates the hide
+    $("#crowd").html("<audio autoplay loop><source src='audio/readyforthis.mp3' type='audio/mpeg'></audio>"); //used jquery to append html to the crowd
 
-//        set the board to row and column in array;
-    board[row][col] = currentMark;
-//        console the win check
-    console.log("board value: " + board[row][col]);
-    if(player1turn){
-        player1score += 3;
-        $(".home .value").text(player1score);
-    }
-    else{
-        player2score += 3;
-        $(".away .value").text(player2score);
-    }
+    setTimeout(function(){
+        var img = $('<div>',{
+            html: "<image id='backc' src='Images/crowd2.png'><image id='frontc' src='Images/crowd1.png'>"
+        })
+        var img2 = $('<div>',{
+            html: "<image id='backc' src='Images/crowd2.png'><image id='frontc' src='Images/crowd1.png'>"
+        })
+        $("#crowd").append(img);
+        $("#crowd2").append(img2);
+        $(".board").css("height", "67vh");
+        $(".board").addClass("lights");
 
 
-    if (checkWin(board)) {
-        $('.board').html('YOU WIN');
-        winAnimation();
-    }
-//        switch the symbols for player turn;
-    togglePlayerSymbols();
+    }, 2000)
 }
 
+function removeWinAnimation() { //removes the winning display
+    $("#crowd").empty();  //jquery method .empty removes all child elements from the selectors
+    $("#crowd2").empty();
+    $(".board").empty("");
+    $(".board").removeClass("lights").removeAttr("height");
+    $(".message h1").html('');
+    player1score = 0;  //added player1score and player2scores to reset score
+    player2score = 0;
+    $(".home .value").text(player1score);
+    $(".away .value").text(player2score);
+    clearInterval(countdownClock);//calls clearInterval function with parameter countdownClock
+    notStarted = true;
+    $("#alert").show();
 
-function togglePlayerSymbols(){
-//        if player 1 turn
-    if(player1turn){
-//            set current mark and symbol to player2 and toggle player boolean
-        currentMark = player2mark;
-        currentSymbol = player2Symbol;
-        player1turn = false;
-        $('.away').removeClass('current_team');
-        $('.home').addClass('current_team');
-    }
-//        must be players 2 turn
-    else{
-//            switch back current mark/symbol to player 1 and toggle player boolean;
-        currentMark = player1mark;
-        currentSymbol = player1Symbol;
-        player1turn = true;
-        $('.home').removeClass('current_team');
-        $('.away').addClass('current_team');
-
-    }
 }
 
 function createBoxes(num){
@@ -149,6 +159,25 @@ function createBoxes(num){
         }
     }
 }
+
+
+
+function createBoardArray(number){
+    var gameArray = [];
+
+    for (var i = 0; i < number; i++) {
+        var pushArray = [];
+        //gameArray.push(pushArray);
+        for(var j = 0; j < number; j++) {
+
+            pushArray.push("");
+        }
+
+        gameArray.push(pushArray);
+    }
+    return gameArray;
+}
+
 function boxDimensions(number,box){
     var size = (parseInt(100/number) -.4) + "%";
     box.css({"width":size,"height":size});
